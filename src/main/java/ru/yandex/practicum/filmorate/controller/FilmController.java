e10bbdc939ba3cf5476653613477362b0cec9ed4
n1k228 <nikitabutov694@gmail>
2025-07-31 20:13:44 +0300

    Первая версия десятого проекта,исправил путь, исправил обновление фильмов и пользователей, теперь новый объект после прохождения валидации заменяет старый ещё убрал слеш

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @Slf4j
 @RestController
-@RequestMapping("/films/")
+@RequestMapping("/films")
 public class FilmController {
     HashMap<Integer, Film> filmMap = new HashMap<>();
 
